473984e5eb93d6389c62ad6335b02dd1140284ff
Renato Sesso <renatohenriquesesso@hotmail.com>
2024-09-17 17:17:10 -0300

    Backend da aplicação

---
+++ backend/coments.js
@@ -0,0 +1,30 @@
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação do backend
+ * POST: Criar uma informação no backend
+ * PUT: Alterar uma informação no backend
+ * DELETE: Deletar uma informação no backend
+ */
+
+/**
+ * Tipos de paramentros:
+ * 
+ * Query Params: Parametros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parametros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos.
+ */
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
